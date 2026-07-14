69e6f0b6ce4451cb07e27994b5b6f43ddbdb3b10
carbridgefr-glitch <carbridgefr@gmail.com>
2026-07-14 11:37:53 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,10 @@
-const CACHE_NAME = "speedfactory-v10";
+const CACHE_NAME = "speedfactory-v12";
 
 const CORE_FILES = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
   "./speed-factory.vcf",
-  "./cover%20no%20backround.png",
-  "./logo%20git%20hub.png",
   "./icon-192.webp",
   "./icon-512.png"
 ];
@@ -15,11 +13,9 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) =>
-        Promise.allSettled(
-          CORE_FILES.map((file) => cache.add(file))
-        )
-      )
+      .then((cache) => Promise.allSettled(
+        CORE_FILES.map((file) => cache.add(file))
+      ))
       .then(() => self.skipWaiting())
   );
 });
@@ -28,43 +24,31 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
       .keys()
-      .then((cacheNames) =>
-        Promise.all(
-          cacheNames
-            .filter((cacheName) => cacheName !== CACHE_NAME)
-            .map((cacheName) => caches.delete(cacheName))
-        )
-      )
+      .then((names) => Promise.all(
+        names
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      ))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") {
-    return;
-  }
+  if (event.request.method !== "GET") return;
 
   const requestUrl = new URL(event.request.url);
-
-  if (requestUrl.origin !== self.location.origin) {
-    return;
-  }
+  if (requestUrl.origin !== self.location.origin) return;
 
   if (event.request.mode === "navigate") {
     event.respondWith(
       fetch(event.request)
         .then((response) => {
           const copy = response.clone();
-
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put("./index.html", copy);
-          });
-
+          caches.open(CACHE_NAME).then((cache) => cache.put("./index.html", copy));
           return response;
         })
         .catch(() => caches.match("./index.html"))
     );
-
     return;
   }
 
@@ -72,18 +56,10 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cachedResponse) => {
       const networkResponse = fetch(event.request)
         .then((response) => {
-          if (
-            response &&
-            response.status === 200 &&
-            response.type === "basic"
-          ) {
+          if (response && response.status === 200 && response.type === "basic") {
             const copy = response.clone();
-
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, copy);
-            });
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
           }
-
           return response;
         })
         .catch(() => cachedResponse);
